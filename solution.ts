092b6b26f66e0ed9261dc88fe72f56c280bbf317
M-H-Nayem <mahmudulhasannayemssnic@gmail.com>
2025-11-18 13:50:12 +0600

    problem 3,4,5,6 solved

--- solution.ts
+++ solution.ts
@@ -28,5 +28,69 @@ function getLength(value: string | number[]): number {
 }
 
 
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails(): string {
+    return `'Name: ${this.name}, Age: ${this.age}';`;
+  }
+}
+
+
+
+function filterByRating(
+  items: {
+    title: string;
+    rating: number;
+  }[]
+): {
+  title: string;
+  rating: number;
+}[] {
+  return items.filter((item) => item.rating >= 4);
+}
+
+
+
+function filterActiveUsers(users: {
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+}[]): {
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+}[] {
+  return users.filter((user) => user.isActive === true);
+}
+
+
+
+
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+function printBookDetails(book: Book): any {
+  const availability = book.isAvailable ? "Yes" : "No";
+  console.log(
+    "Title: " + book.title +
+    ", Author: " + book.author +
+    ", Published: " + book.publishedYear +
+    ", Available: " + availability
+  );
+}
 
 
